97e5f2a13cb6359762be4df96190a97e5f7291de
sumitra-source <shresthasumitra1998@gmail.com>
2020-09-29 09:31:16 +0545

    update upto 24.09.2020

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ASP.NETmvc.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace ASP.NETmvc.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IConfiguration _iConfig;
+        public HomeController(ILogger<HomeController> logger, IConfiguration iConfig)
+        {
+            _logger = logger;
+            _iConfig = iConfig;
+
+
+
+            //add items to list
+            //hari as a doctor
+            //inherit from profiledetail
+
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.MyKey = _iConfig["MyKey"];
+            return View();
+
+        }
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Persons()
+        {
+            return View(PersonMemory.GetPersons());
+
+        }
+        public IActionResult PersonalDetails(int personDetailId)
+        {
+            PersonalDetail pdetail = new PersonalDetail();
+            pdetail = PersonMemory.GetPersons().Where(x => x.PersonalDetailId == personDetailId).FirstOrDefault();
+
+            return View(pdetail);
+        }
+
+        public IActionResult EditPersonalDetail(int personDetailId)
+        {
+            PersonalDetail pdetail = new PersonalDetail();
+            pdetail = PersonMemory.GetPersons().Where(x => x.PersonalDetailId == personDetailId).FirstOrDefault();
+
+            return View(pdetail);
+        }
+
+
+        [HttpPost]
+        public IActionResult EditPersonalDetail(PersonalDetail personDetail)
+        {
+            var personInList = PersonMemory.GetPersons().Where(x => x.PersonalDetailId == personDetail.PersonalDetailId).FirstOrDefault();
+
+            personInList.FirstName = personDetail.FirstName;
+            personInList.Occupation = personDetail.Occupation;
+            personInList.Age = personDetail.Age;
+            personInList.Address = personDetail.Address;
+
+            return RedirectToAction("Persons");
+        }
+
+        public IActionResult CreatePerson()
+        {
+            PersonalDetail person = new PersonalDetail();
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePerson(PersonalDetail person)
+        {
+            var personDetailList = PersonMemory.GetPersons();
+            int personsCount = personDetailList.Count;
+            person.PersonalDetailId = ++personsCount;
+
+            personDetailList.Add(person);
+
+            return RedirectToAction("Persons");
+        }
+
+        public IActionResult DeletePerson(int personDetailId)
+        {
+            var personInList = PersonMemory.GetPersons().Where(person => person.PersonalDetailId == personDetailId).FirstOrDefault();
+            PersonMemory.GetPersons().Remove(personInList);
+
+            return RedirectToAction("Persons");
+        }
+
+
+    }
+}
---
+++ Models/PersonDetail.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETmvc.Models
+{
+    public class PersonalDetail
+    {
+        public PersonalDetail()
+        {
+        }
+
+        // properties
+        public int PersonalDetailId { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+
+    }
+
+    /* public class DoctorProfile : PersonalDetail
+    {
+        public DoctorProfile(List<PersonalDetail> persons) : base(persons)
+        {
+        }
+        public string Qualification { get; set; }
+        public string HospitalClinic { get; set; }
+        public string Speciality { get; set; }
+    }
+    public class TeacherProfile : PersonalDetail
+    {
+        public TeacherProfile(List<PersonalDetail> persons) : base(persons)
+        {
+        }
+        public string Qualification { get; set; }
+        public string College { get; set; }
+        public string AssociatedSubjects { get; set; }
+    }
+    public class StudentProfile : PersonalDetail
+    {
+        public StudentProfile(List<PersonalDetail> persons) : base(persons)
+        {
+        }
+        public string Faculty { get; set; }
+        public string College_School { get; set; }
+        public string Rollno { get; set; }
+    } */
+
+
+
+}
