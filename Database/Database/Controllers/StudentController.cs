885b48611ef839b54c2a8de08a25dc9843910bb1
sumitra-source <shresthasumitra1998@gmail.com>
2020-12-07 21:55:36 +0545

    before EF

--- Database/Database/Controllers/StudentController.cs
+++ Database/Database/Controllers/StudentController.cs
@@ -14,20 +14,58 @@ namespace Database.Controllers
         public IActionResult ClassIndex()
         {
             // C: \Users\dell\source\repos\TestingNetNetCore\DAL\Student.mdf
-            string datasource = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\shres\source\repos\Database\DaL\StudentManage.mdf;
-Integrated Security=True;
-                          Connect Timeout=30;
-                          User Instance=True";
+            // string datasource = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\shres\Desktop\ASP.NET\Database\DaL\EmpManagement.mdf;Integrated Security=True;
+            //  "
+            // ;
+            // string datasource = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\shres\Desktop\ASP.NET\Database\DaL\StudentManagement.mdf; Integrated Security = True;
+            //  ";
+            SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-6P6C4JH;database=Employeemanagement;Integrated Security=True");
             List<ClassModel> classes = new List<ClassModel>();
-            SqlConnection sqlConnection = new SqlConnection(datasource);
+            // SqlConnection sqlConnection = new SqlConnection(datasource);
             //SqlConnection sqlConnection = new SqlConnection("Data Source=Ramesh-Pc;database=SchoolBasu;Integrated Security=True");
             sqlConnection.Open();
-            SqlCommand sqlCommand = new SqlCommand("Select * from Classes", sqlConnection);//it is a sql command to execute in database
+            SqlCommand sqlCommand = new SqlCommand("Select * from Empdetail", sqlConnection);//it is a sql command to execute in database
             SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // its a buffer to hold data from database
             DataTable dataTable = new DataTable(); //its is data type, like integer
             sqlDataAdapter.Fill(dataTable); //fill the datatable from the buffer
             sqlConnection.Close();
             return View(dataTable);
         }
+   
+      public IActionResult AddUpdateClass(int? classId)
+        {
+            ClassModel classModel = new ClassModel();
+
+            return View(classModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddUpdateClass(ClassModel classModel)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-6P6C4JH;database=Employeemanagement;Integrated Security=True"))
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("Insert into Empdetail(emp_name,emp_id,emp_address) values ('" + classModel.ClassName + "','" + classModel.ClassId + "','" + classModel.ClassTeacher +"' )", sqlConnection);//it is a sql command to execute in database
+                int affectedRows = sqlCommand.ExecuteNonQuery();
+                //sqlConnection.Close();
+                return RedirectToAction("ClassIndex");
+            }
+        }
+
+
+       // public IActionResult Students()
+       // {
+            //List<BMCStudentModel> students = context.Students.Select(x => new BMCStudentModel()
+            //{
+            //    StudentId=x.StudentId,
+            //    StudentName = x.StudentName,
+            //    Address=x.Address,
+            //    Batch=x.Batch,
+            //    ContactNumber=x.ContactNumber
+            //}
+            //).ToList();
+
+         //   List<Student> students = context.Students.ToList();
+           // return View(students);
+        }
     }
-}
